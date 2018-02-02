14c81fac0ff1119a3f54e35c2b463bd77020fe1d
Julian Orozco <orozco.castorena@outlook.com>
2018-02-01 20:50:49 -0600

    Create listings finished and Dashboard show listings.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Dashboard <span class="pull-right"><a href="/listings/create" class="bnt btn-success btn-xs">Add Listing</a></span></div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,10 +13,25 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <h3>Your Listings</h3>
+                    @if(count($listings))
+                    <table class="table table-striped">
+                    <tr>
+                    <th>Company</th>
+                    <th>Website</th>
+                    <th>Phone</th>
+                    </tr>
+                    @foreach($listings as $listing)
+                    <tr>
+                    <td>{{$listing->name}}</td>
+                    <td>{{$listing->website}}</td>
+                    <td>{{$listing->phone}}</td>
+                    </tr>
+                    @endforeach
+                    </table>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
